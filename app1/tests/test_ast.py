2d4b6c4ad8012be40a5c6f7e12c8872325bfd9d8
Vaibhav Jain <jainvaibhav671@gmail.com>
2024-10-26 22:10:52 +0530

    Rearranging the code folders

--- app1/tests/test_ast.py
+++
@@ -1,9 +0,0 @@
-from zeotap.ast import *
-
-def test_parse_rule_1():
-    rule = "((age > 30 AND department = 'Sales') OR (age < 25 AND department = 'Marketing')) AND (salary > 50000 OR experience > 5)"
-    rule_tree = combine_rules([rule])
-    level_order(rule_tree)
-
-def test_parse_rule_2():
-    rule = "((age > 30 AND department = 'Marketing')) AND (salary > 20000 OR experience > 5)"
